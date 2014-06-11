720d99adf8b23067e31040306787d0e4dccda688
Roger <roger.chen@kindminds.com>
2014-06-11 13:44:04 +0800

    2nd Commit

---
+++ drs_product/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': "",
+    'version': "1.0",
+    'author': "Roger@Kmi",
+    'category': "KindMinds Innovations",
+    "depends": [
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
